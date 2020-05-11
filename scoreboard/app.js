0b54755052231817cf084b5883d008ab08cd7234
Stuuu <stuartschrick@gmail.com>
2020-05-10 21:29:42 -0400

    react lesson 1 done

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -1,14 +1,14 @@
-const title = <h1>My first React Element! </h1>;
-const desc = <p>I just learned how to create a React node and render it into the Dom</p>
-console.log(title);
+const desc = 'I just learned how to create a React node and render it into the Dom'
+const myTitleID = 'main-title';
+const name = 'Stuart';
 
-
-const header = React.createElement(
-    'header',
-    null,
-    title,
-    desc
-);
+const header = (
+    <header>
+    {/* this is a comment */}
+        <h1 id={myTitleID}>{ name }'s First react element</h1>
+        <p className="main-desc">{ desc }</p>
+    </header>
+)
 
 
 
