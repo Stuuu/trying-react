7283fb28ab9de219497731097191fb567df53e18
Stuuu <stuartschrick@gmail.com>
2020-05-10 21:35:33 -0400

    Creating components

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -1,14 +1,11 @@
-const desc = 'I just learned how to create a React node and render it into the Dom'
-const myTitleID = 'main-title';
-const name = 'Stuart';
-
-const header = (
-    <header>
-    {/* this is a comment */}
-        <h1 id={myTitleID}>{ name }'s First react element</h1>
-        <p className="main-desc">{ desc }</p>
-    </header>
-)
+function Header() {
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">Players: 1</span>
+        </header>
+    );
+}
 
 
 
